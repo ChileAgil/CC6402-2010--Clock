a8588e20fa4bea815eb7cb1462fde787dfccb9d5
Pedro Valencia <pvalencia@gmail.com>
2010-08-27 12:06:13 -0400

    reescrito el test por vendida

--- testTimeModel.rb
+++ testTimeModel.rb
@@ -1,52 +1,72 @@
-class TimeModel
-    
-    SECONDS_OF_MINUTE = 60
-    MINUTES_OF_HOUR = 60
-    HOURS_OF_DAY = 24
+require 'test/unit'
+require "TimeModel.rb"
 
-    attr_reader :hours
-    attr_reader :minutes
-    attr_reader :seconds
-    
-    def initialize(hour = 0, minute = 0, second = 0)
-        if (hour < 0 ) or (hour >= HOURS_OF_DAY)
-           raise ArgumentError
-        end
-        if (minute < 0 ) or (minute >= MINUTES_OF_HOUR)
-           raise ArgumentError
-        end
-        if (second < 0 ) or (second >= SECONDS_OF_MINUTE)
-           raise ArgumentError
-        end
-        
-        @hours = hour
-        @minutes = minute
-        @seconds = second
+class TimeModelTest < Test::Unit::TestCase
+    def test_initialize
+        t = TimeModel.new(1,2,3)
+        assert_equal(1, t.hours)
+        assert_equal(2, t.minutes)
+        assert_equal(3, t.seconds)
+        t = TimeModel.new()
+        assert_equal(0, t.hours)
+        assert_equal(0, t.minutes)
+        assert_equal(0, t.seconds)
+	assert_equal([0,0,0], t.current_time)
     end
-    
-    def current_time
-        [@hours, @minutes, @seconds]
-    end
-    
-    def second_passed
-        @seconds = (@seconds+1)%SECONDS_OF_MINUTE
-        if @seconds == 0
-            minute_passed
+	
+    def test_exceptions
+        assert_raise ArgumentError do
+            t = TimeModel.new(24, 0, 0)
         end
-    end
-    
-    def minute_passed
-        @minutes = (@minutes+1)%MINUTES_OF_HOUR
-        if @minutes == 0
-            hour_passed
+        assert_raise ArgumentError do
+            t = TimeModel.new(23, 60, 0)
         end
+        assert_raise ArgumentError do
+            t = TimeModel.new(23, 59, 60)
+        end
+        assert_raise ArgumentError do
+            t = TimeModel.new(-1, 0, 0)
+        end
+        assert_raise ArgumentError do
+            t = TimeModel.new(23, -1, 0)
+        end
+        assert_raise ArgumentError do
+            t = TimeModel.new(23, 59, -1)
+        end
+    end    
+
+    def test_second_passed
+        t = TimeModel.new(0,0,0)
+        t.second_passed
+        assert_equal(0, t.hours)
+        assert_equal(0, t.minutes)
+        assert_equal(1, t.seconds)
     end
-    
-    def hour_passed
-        @hours = (@hours+1)%HOURS_OF_DAY
+    def test_minute_passed
+        t = TimeModel.new(0,0,59)
+        t.second_passed()
+        assert_equal(0, t.hours)
+        assert_equal(1, t.minutes)
+        assert_equal(0, t.seconds)
     end
-    
-    def to_s
-        "%02d:%02d:%02d" % [@hours, @minutes, @seconds]
+    def test_hour_passed
+        t = TimeModel.new(0,59,59)
+        t.second_passed()
+        assert_equal(1, t.hours)
+        assert_equal(0, t.minutes)
+        assert_equal(0, t.seconds)
+    end
+    def test_day_passed
+        t = TimeModel.new(23,59,59)
+        t.second_passed()
+        assert_equal(0, t.hours)
+        assert_equal(0, t.minutes)
+        assert_equal(0, t.seconds)
+    end
+    def test_to_s
+        t = TimeModel.new
+        assert_equal("00:00:00", t.to_s)
+        t = TimeModel.new(23,59,59)
+        assert_equal("23:59:59", t.to_s)
     end
 end
